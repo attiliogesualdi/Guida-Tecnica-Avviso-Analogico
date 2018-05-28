5cbe478617e364a9aff9da71380572c362d64bee
attiliogesualdi <38068134+attiliogesualdi@users.noreply.github.com>
2018-05-28 16:09:16 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -2,13 +2,13 @@
 # -*- coding: utf-8 -*-
 
 # -- PROJECT Variables ----------------------------------------------------
-settings_project_name = "GuidaTecnica_AvvisoAnalogico_V2.1"
+settings_project_name = "Guida_Tecnica_Avviso_Analogico_V2.1"
 settings_copyright_copyleft = 'AgID - Team Digitale'
 settings_editor_name = 'AgID - Team Digitale'
 settings_doc_version = 'version: latest'
 settings_doc_release = 'version: latest'
-settings_basename = 'GuidaTecnica_AvvisoAnalogico_V2.1'
-settings_file_name = 'GuidaTecnica_AvvisoAnalogico_V2.1'
+settings_basename = 'Guida_Tecnica_Avviso_Analogico_V2.1'
+settings_file_name = 'Guida_Tecnica_Avviso_Analogico_V2.1'
 
 # -- No need to change below here
 
